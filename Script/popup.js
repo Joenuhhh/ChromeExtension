2123df0503c3fe11a7829314a6ff01b741c80a5b
Joenuhhh <79020295+Joenuhhh@users.noreply.github.com>
2023-12-13 22:54:12 -0700

    Teacher settings pages rough draft added.

--- Script/popup.js
+++ Script/popup.js
@@ -1,22 +1,40 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var activeTab = tabs[0];
-    chrome.scripting.executeScript({
+  var activeTab = tabs[0];
+  chrome.scripting.executeScript({
       target: { tabId: activeTab.id },
       function: function () {
-        // Expand collapsible content
-        var collapsibleDivs = document.querySelectorAll('.collapsible'); // Replace '.collapsible' with the correct selector
-        collapsibleDivs.forEach(function (div) {
-          div.style.display = 'block'; // You might need to set other styles as well
-        });
-  
-        // Convert the document object to a formatted HTML string
-        var serializer = new XMLSerializer();
-        var prettyHtml = serializer.serializeToString(document);
-        // Add a newline after every closing bracket
-        prettyHtml = prettyHtml.replace(/>/g, '>\n');
-        // Print the formatted HTML to the console
-        console.log(prettyHtml);
+          var studentList = []; // Create an empty array to store student data
+
+          // Get the table rows from the document
+          var tableRows = document.querySelectorAll('table tbody tr');
+
+          // Iterate through each table row
+          tableRows.forEach(function (row) {
+              var columns = row.querySelectorAll('td'); // Get all columns in the row
+
+              // Extract data from each column
+              var id = columns[0].textContent;
+              var firstName = columns[1].textContent;
+              var lastName = columns[2].textContent;
+              var grade1 = columns[3].textContent;
+              var grade2 = columns[4].textContent;
+              var grade3 = columns[5].textContent;
+              var grade4 = columns[6].textContent;
+              var grade5 = columns[7].textContent;
+
+              // Create a student object and add it to the student list
+              var student = {
+                  id: id,
+                  firstName: firstName,
+                  lastName: lastName,
+                  grades: [grade1, grade2, grade3, grade4, grade5],
+              };
+
+              studentList.push(student);
+          });
+
+          // Print the list of students to the console
+          console.log(studentList);
       },
-    });
   });
-  
+});
